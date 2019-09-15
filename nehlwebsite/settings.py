f97258ab95e6d9c0b5c7047775d6ac6980705007
Daniel Baxter-Beard <d.h.baxter-beard1@ncl.ac.uk>
2019-09-15 23:39:28 +0100

    fix dependencies

--- nehlwebsite/settings.py
+++ nehlwebsite/settings.py
@@ -145,6 +145,7 @@ LOGIN_REDIRECT_URL = '/accounts/profile'
 
 EMAIL_BACKEND = 'django_ses.SESBackend'
 AWS_SES_REGION_NAME = 'eu-west-1'
+AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
 
 # you run `collectstatic`).
 

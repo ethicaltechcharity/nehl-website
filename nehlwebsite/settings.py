9efe3502a8ea8a5bbb441f7d75769192aa3d060b
Daniel Baxter-Beard <d.h.baxter-beard1@ncl.ac.uk>
2019-10-02 16:32:29 +0100

    Cancelled by

--- nehlwebsite/settings.py
+++ nehlwebsite/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'nehlwebsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+#
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -94,7 +94,6 @@ DATABASES = {
     }
 }
 
-# if DEBUG:
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
